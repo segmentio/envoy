48a46205756b848cf400301c65b2a4c3d6e636d2
Daniel Fuentes <daniel.fuentes@segment.com>
2018-10-30 23:47:34 -0700

    run code formatter

--- source/extensions/filters/http/grpcerrors/config.h
+++ source/extensions/filters/http/grpcerrors/config.h
@@ -6,19 +6,20 @@
 #include "extensions/filters/http/well_known_names.h"
 
 namespace Envoy {
-  namespace Extensions {
-    namespace HttpFilters {
-      namespace GrpcErrors {
-        class GrpcErrorsFilterConfig
-          : public Common::FactoryBase<envoy::config::filter::http::grpcerrors::v2::Config> {
-        public:
-          GrpcErrorsFilterConfig() : FactoryBase(HttpFilterNames::get().GrpcErrors) {}
-        private:
-          Http::FilterFactoryCb createFilterFactoryFromProtoTyped(
-                                                                  const envoy::config::filter::http::grpcerrors::v2::Config& proto_config,
-                                                                  const std::string& stats_prefix, Server::Configuration::FactoryContext& context) override;
-        };
-      }
-    } // namespace HttpFilters
-  } // namespace Extensions
+namespace Extensions {
+namespace HttpFilters {
+namespace GrpcErrors {
+class GrpcErrorsFilterConfig
+    : public Common::FactoryBase<envoy::config::filter::http::grpcerrors::v2::Config> {
+public:
+  GrpcErrorsFilterConfig() : FactoryBase(HttpFilterNames::get().GrpcErrors) {}
+
+private:
+  Http::FilterFactoryCb createFilterFactoryFromProtoTyped(
+      const envoy::config::filter::http::grpcerrors::v2::Config& proto_config,
+      const std::string& stats_prefix, Server::Configuration::FactoryContext& context) override;
+};
+} // namespace GrpcErrors
+} // namespace HttpFilters
+} // namespace Extensions
 } // namespace Envoy
--- source/extensions/filters/http/grpcerrors/grpcerrors_filter.h
+++ source/extensions/filters/http/grpcerrors/grpcerrors_filter.h
@@ -9,53 +9,54 @@
 #include "common/common/logger.h"
 
 namespace Envoy {
-  namespace Extensions {
-    namespace HttpFilters {
-      namespace GrpcErrors {
+namespace Extensions {
+namespace HttpFilters {
+namespace GrpcErrors {
 
-        class Config : public Logger::Loggable<Logger::Id::config> {
-        public:
-          Config(const envoy::config::filter::http::grpcerrors::v2::Config config);
+class Config : public Logger::Loggable<Logger::Id::config> {
+public:
+  Config(const envoy::config::filter::http::grpcerrors::v2::Config config);
 
-          std::string error_key() const { return error_key_; }
-          bool show_grpc_error_code() const { return show_grpc_error_code_; }
+  std::string error_key() const { return error_key_; }
+  bool show_grpc_error_code() const { return show_grpc_error_code_; }
 
-        private:
-          std::string error_key_{"error"};
-          bool show_grpc_error_code_{false};
-        };
+private:
+  std::string error_key_{"error"};
+  bool show_grpc_error_code_{false};
+};
 
-        typedef std::shared_ptr<Config> ConfigSharedPtr;
+typedef std::shared_ptr<Config> ConfigSharedPtr;
 
-        class GrpcErrorsFilter : public Http::StreamEncoderFilter,
-                                 public Logger::Loggable<Logger::Id::filter> {
-        public:
-          GrpcErrorsFilter(const ConfigSharedPtr config);
-          ~GrpcErrorsFilter();
+class GrpcErrorsFilter : public Http::StreamEncoderFilter,
+                         public Logger::Loggable<Logger::Id::filter> {
+public:
+  GrpcErrorsFilter(const ConfigSharedPtr config);
+  ~GrpcErrorsFilter();
 
-          void onDestroy() override;
+  void onDestroy() override;
 
-          // StreamEncoderFilter
-          Http::FilterHeadersStatus encode100ContinueHeaders(Http::HeaderMap&) override {
-            return Http::FilterHeadersStatus::Continue;
-          }
-          Http::FilterDataStatus encodeData(Buffer::Instance&, bool) override {
-            return Http::FilterDataStatus::Continue;
-          }
-          Http::FilterTrailersStatus encodeTrailers(Http::HeaderMap&) override {
-            return Http::FilterTrailersStatus::Continue;
-          }
-          void setEncoderFilterCallbacks(Http::StreamEncoderFilterCallbacks& callbacks) override {
-            encoder_callbacks_ = &callbacks;
-          }
+  // StreamEncoderFilter
+  Http::FilterHeadersStatus encode100ContinueHeaders(Http::HeaderMap&) override {
+    return Http::FilterHeadersStatus::Continue;
+  }
+  Http::FilterDataStatus encodeData(Buffer::Instance&, bool) override {
+    return Http::FilterDataStatus::Continue;
+  }
+  Http::FilterTrailersStatus encodeTrailers(Http::HeaderMap&) override {
+    return Http::FilterTrailersStatus::Continue;
+  }
+  void setEncoderFilterCallbacks(Http::StreamEncoderFilterCallbacks& callbacks) override {
+    encoder_callbacks_ = &callbacks;
+  }
 
-          Http::FilterHeadersStatus encodeHeaders(Http::HeaderMap& headers, bool) override;
-        private:
-          const ConfigSharedPtr config_;
-          Http::StreamEncoderFilterCallbacks* encoder_callbacks_{};
-        };
+  Http::FilterHeadersStatus encodeHeaders(Http::HeaderMap& headers, bool) override;
 
-      } // namespace GrpcErrors
-    } // namespace HttpFilters
-  } // namespace Extensions
+private:
+  const ConfigSharedPtr config_;
+  Http::StreamEncoderFilterCallbacks* encoder_callbacks_{};
+};
+
+} // namespace GrpcErrors
+} // namespace HttpFilters
+} // namespace Extensions
 } // namespace Envoy
